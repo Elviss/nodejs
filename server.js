045aac65350ae700915f3dd871f57dfaf03e91f6
elvis <elvis.superlogica@gmail.com>
2019-12-11 12:23:24 -0300

    instalacao do yarn e adicionado a dependencia do bootstrap

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Ola Mundo\nMeu Primeiro Script');
-})
+});
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`)
